ac3da5beea51bf35e22d2bbdc13c933a494b10a1
Angelo Pirola <angelo9871@gmail.com>
2023-07-11 08:59:56 +0200

    Implementazioni EFCore

--- src/CustomLibrary.EFCore/EFCore/Infrastructure/Repository/CoreDatabase.cs
+++ src/CustomLibrary.EFCore/EFCore/Infrastructure/Repository/CoreDatabase.cs
@@ -42,16 +42,91 @@ public class CoreDatabase<TEntity, TKey> : Database<TEntity, TKey>, ICoreDatabas
     //    return new ListViewModel<TEntity> { Results = result, TotalCount = result.Count };
     //}
 
-    public Task<List<TEntity>> GetItemsAsync(Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> includes,
-    Expression<Func<TEntity, bool>> condition, CancellationToken cancellationToken = default) => throw new NotImplementedException();
+    public async Task<List<TEntity>> GetItemsAsync(Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> includes,
+    Expression<Func<TEntity, bool>> condition, CancellationToken cancellationToken = default)
+    {
+        IQueryable<TEntity> query = DbContext.Set<TEntity>();
 
-    public Task<TEntity> GetItemByIdAsync(Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> includes,
-        Expression<Func<TEntity, bool>> condition, CancellationToken cancellationToken = default) => throw new NotImplementedException();
+        if (includes != null)
+        {
+            query = includes(query);
+        }
 
-    public Task<List<TEntity>> GetOrderedItemsAsync(Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> includes,
+        if (condition != null)
+        {
+            query = query.Where(condition);
+        }
+
+        return await query.AsNoTracking().ToListAsync(cancellationToken);
+    }
+
+    public async Task<TEntity> GetItemByIdAsync(Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> includes,
+        Expression<Func<TEntity, bool>> condition, CancellationToken cancellationToken = default)
+    {
+        IQueryable<TEntity> query = DbContext.Set<TEntity>();
+
+        if (includes != null)
+        {
+            query = includes(query);
+        }
+
+        if (condition != null)
+        {
+            query = query.Where(condition);
+        }
+
+        return await query.AsNoTracking().FirstOrDefaultAsync(cancellationToken);
+    }
+
+    public async Task<List<TEntity>> GetOrderedItemsAsync(Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> includes,
         Expression<Func<TEntity, bool>> conditionWhere, Expression<Func<TEntity, dynamic>> orderBy,
-        OrderType orderType = OrderType.Ascending, CancellationToken cancellationToken = default) => throw new NotImplementedException();
+        OrderType orderType = OrderType.Ascending, CancellationToken cancellationToken = default)
+    {
+        IQueryable<TEntity> query = DbContext.Set<TEntity>();
 
-    public Task<int> GetItemsCountAsync(Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> includes,
-        Expression<Func<TEntity, bool>> condition, CancellationToken cancellationToken = default) => throw new NotImplementedException();
+        if (includes != null)
+        {
+            query = includes(query);
+        }
+
+        if (conditionWhere != null)
+        {
+            query = query.Where(conditionWhere);
+        }
+
+        if (orderBy != null)
+        {
+            if (orderType == OrderType.Ascending)
+            {
+                query = query.OrderBy(orderBy);
+            }
+
+            if (orderType == OrderType.Descending)
+            {
+                query = query.OrderByDescending(orderBy);
+            }
+        }
+
+        return await query
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<int> GetItemsCountAsync(Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> includes,
+        Expression<Func<TEntity, bool>> condition, CancellationToken cancellationToken = default)
+    {
+        IQueryable<TEntity> query = DbContext.Set<TEntity>();
+
+        if (includes != null)
+        {
+            query = includes(query);
+        }
+
+        if (condition != null)
+        {
+            query = query.Where(condition);
+        }
+
+        return await query.AsNoTracking().CountAsync(cancellationToken);
+    }
 }
